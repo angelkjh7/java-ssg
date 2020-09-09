a7aaed075b7a17b86f29c3c70b8f3265f89d79ed
angelkjh7 <angelkjh5@naver.com>
2020-09-09 17:20:21 +0900

    66

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hi world");
 		System.out.println("Hi world22");
-		System.out.println("Hi world55");
+		System.out.println("Hi world66");
 	}
 }
